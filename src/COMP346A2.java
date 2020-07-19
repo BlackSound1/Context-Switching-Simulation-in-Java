5c64a70aafc2aaa694f3d3770a43200121b39d55
BlackSound1 <matthewr@videotron.ca>
2020-07-19 19:14:00 -0400

    Added the methods we need to implement. Probably need to implement more

--- src/COMP346A2.java
+++ src/COMP346A2.java
@@ -1,5 +1,30 @@
+/**
+ * @author Matthew Segal
+ * @author Laura Boivin
+ *
+ */
 public class COMP346A2 {
     public static void main(String[] args) {
         System.out.println("TEST");
+    }
+
+    private void firstComeFirstServe(){
+
+    }
+
+    private void shortestJobFirst(){
+
+    }
+
+    private void roundRobin(int timeQuantum){
+
+    }
+
+    private void shortestRemainingTimeFirst(){
+
+    }
+
+    private void readFile(){
+
     }
 }
