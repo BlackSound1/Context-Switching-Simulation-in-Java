32b8c8ba9a38fbe6b3874c2e23cfb87dc33e405c
BlackSound1 <matthewr@videotron.ca>
2020-07-19 20:07:14 -0400

    Added Process class and ProcessState enum. Tested the creation and running of a null Process

--- src/COMP346A2.java
+++ src/COMP346A2.java
@@ -6,6 +6,8 @@
 public class COMP346A2 {
     public static void main(String[] args) {
         System.out.println("TEST");
+        Thread TESTTHREAD = new Thread(new Process());
+        TESTTHREAD.start();
     }
 
     private void firstComeFirstServe(){
