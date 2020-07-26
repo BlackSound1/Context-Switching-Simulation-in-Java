3012aebf7e9e3cec75ede7f04c07eb2e518d8cac
BlackSound1 <matthewr@videotron.ca>
2020-07-26 11:12:09 -0400

    Finished RR. Did some organizing

--- src/CPU.java
+++ src/CPU.java
@@ -3,21 +3,21 @@ public class CPU  {
     private int CPUID;
     private CPUState state;
     private int utilization;
+    private int timeQuantumTimer;
 
     public CPU() {
         process = null;
         CPUID = -1;
         state = CPUState.BUSY;
         utilization = 0;
+        timeQuantumTimer = 0;
     }
 
     public CPU(Process process) {
         this.process = process;
     }
 
-    public Process getProcess() {
-        return process;
-    }
+    public Process getProcess() { return process; }
 
     public void setProcess(Process process) {
         this.process = process;
@@ -42,6 +42,10 @@ public class CPU  {
     public int getUtilization() { return utilization; }
 
     public void setUtilization(int utilization) { this.utilization = utilization; }
+
+    public int getTimeQuantumTimer() { return timeQuantumTimer; }
+
+    public void setTimeQuantumTimer(int timeQuantum) { this.timeQuantumTimer = timeQuantum; }
 
     @Override
     public String toString() {
